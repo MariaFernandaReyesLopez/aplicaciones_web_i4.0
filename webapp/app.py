6df9dae47e8ca918cf62eb9be005e796d71ae42e
MariaFernandaReyesLopez <1718110411@utectulancingo.edu.mx>
2020-07-31 23:48:38 -0500

    Controllers y url listos

---
+++ webapp/app.py
@@ -0,0 +1,15 @@
+import web
+
+urls = (
+    '/', 'mvc.controllers.index.Index',
+    
+    '/personas_list', 'mvc.controllers.personas.list.List',
+    '/personas_delete','mvc.controllers.personas.delete.Delete',
+    '/personas_insert','mvc.controllers.personas.insert.Insert',
+    '/personas_update','mvc.controllers.personas.update.Update',
+    '/personas_view/(.*)','mvc.controllers.personas.view.View',
+)
+app = web.application(urls, globals())
+
+if __name__ == "__main__":
+    app.run()
